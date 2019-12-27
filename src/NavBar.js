8ad30643736b8cc46d23aaa1016e42cd299efa11
Zlatoslav <z.marchev@abv.bg>
2019-12-27 11:12:43 +0200

    Emotion

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,18 +1,36 @@
 import React from 'react';
 import { Link } from '@reach/router';
-import { css } from '@emotion/core';
+import { css, keyframes } from '@emotion/core';
+import colors from './colors';
 
-const NavBar = () => (
-    <header
-        css={css`
-            background-color: #333;
+const spin = keyframes`
+    to {
+        transform: rotate(360deg);
+    }
+`
+
+const NavBar = () => {
+    return (
+        <header
+            css={css`
+            background-color: ${colors.secondary};
             padding: 15px;
 
         `}
-    >
-        <Link to="/">Adopt Me!</Link>
-        <span role="img" aria-label="logo">:)</span>
-    </header>
-)
+        >
+            <Link to="/">Adopt Me!</Link>
+            <span css={css`
+                font-size: 60px;
+                display: inline-block;
+                animation: 1s ${spin} linear infinite;
 
+                &:hover {
+                    animation: 1s ${spin} linear infinite reverse;
+                    text-decoration: underline;
+                }
+            `}
+                role="img" aria-label="logo">:)</span>
+        </header>
+    )
+}
 export default NavBar;
