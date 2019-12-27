557e88768219a2a734c1fcd5b3cd7fc8b85ba840
Zlatoslav <z.marchev@abv.bg>
2019-12-27 11:20:03 +0200

    Revert "Emotion"
    This reverts commit 8ad30643736b8cc46d23aaa1016e42cd299efa11.

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,36 +1,18 @@
 import React from 'react';
 import { Link } from '@reach/router';
-import { css, keyframes } from '@emotion/core';
-import colors from './colors';
+import { css } from '@emotion/core';
 
-const spin = keyframes`
-    to {
-        transform: rotate(360deg);
-    }
-`
-
-const NavBar = () => {
-    return (
-        <header
-            css={css`
-            background-color: ${colors.secondary};
+const NavBar = () => (
+    <header
+        css={css`
+            background-color: #333;
             padding: 15px;
 
         `}
-        >
-            <Link to="/">Adopt Me!</Link>
-            <span css={css`
-                font-size: 60px;
-                display: inline-block;
-                animation: 1s ${spin} linear infinite;
+    >
+        <Link to="/">Adopt Me!</Link>
+        <span role="img" aria-label="logo">:)</span>
+    </header>
+)
 
-                &:hover {
-                    animation: 1s ${spin} linear infinite reverse;
-                    text-decoration: underline;
-                }
-            `}
-                role="img" aria-label="logo">:)</span>
-        </header>
-    )
-}
 export default NavBar;
